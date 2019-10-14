28de346fba193618b9b2f2bfd1b27bae3b57ecbb
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-10-14 13:34:31 +0900

    Postテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_14_043125) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
