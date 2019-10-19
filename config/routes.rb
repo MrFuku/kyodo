9b068a957ea55acb9cad2091efaddcf2af7fff87
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-10-19 22:16:43 +0900

    投稿保存のためのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
   
   get '/' => 'home#top'
   get '/about' => 'home#about'
