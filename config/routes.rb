94258d73180375a42cc09b615138a356c247047a
kawac2236 <kawac2236@gmail.com>
2019-10-20 21:03:11 +0900

    削除機能の追加 （destroyメソッド）

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post 'posts/create' => 'posts#create'
   get 'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+  
   get '/' => 'home#top'
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
