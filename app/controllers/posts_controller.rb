9dc244b74cd14b12561f806465e3b3c7327d726e
kawac2236 <kawac2236@gmail.com>
2019-10-20 20:47:27 +0900

    更新機能の追加 （edit,updateメソッド）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,19 @@ class PostsController < ApplicationController
   def new
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
   def create
     @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to('/posts/index')
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
     @post.save
     redirect_to('/posts/index')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
   post 'posts/create' => 'posts#create'
-  
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
   get '/' => 'home#top'
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
