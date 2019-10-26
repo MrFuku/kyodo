d4b551bffda5f066ad17d98813f61224aeb1edff
u-yah <yuyahayashi3@gmail.com>
2019-10-26 23:06:09 +0900

    新規投稿成功時と投稿削除成功時にflashを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(content: params[:content])
     if @post.save
+      flash[:notice] = "投稿を作成しました"
       redirect_to('/posts/index')
     else
       render('/posts/new')
@@ -38,6 +39,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました"
     redirect_to('/posts/index')
   end
 end
