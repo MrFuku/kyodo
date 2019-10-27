30d223d64d5494a0ff805f94c844f1b7a2c6face
kawac2236 <kawac2236@gmail.com>
2019-10-27 22:09:39 +0900

    createをroutesに設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
   get 'signup' => 'users#new'
+  post 'users/create' => 'users#create'
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
