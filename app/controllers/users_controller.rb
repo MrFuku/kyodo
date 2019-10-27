78bc070264e30c882896256abcc489a116d433e6
kawac2236 <kawac2236@gmail.com>
2019-10-27 22:09:55 +0900

    createメソッドの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new(name: params[:name], email: params[:email])
+    user.save
+    redirect_to("/users/#{user.id}")
+  end
 end
