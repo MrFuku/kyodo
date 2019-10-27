5f26225723a904e97b33636db0a1fc6562b1c811
kawac2236 <kawac2236@gmail.com>
2019-10-27 22:24:37 +0900

    createメソッドにエラーハンドリング追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(name: params[:name], email: params[:email])
-    user.save
-    redirect_to("/users/#{user.id}")
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{user.id}")
+    else
+      render("/users/new")
+    end
   end
 end
