463642b15d422575ca9597ac902013a99927faaf
kawac2236 <kawac2236@gmail.com>
2019-10-27 21:55:00 +0900

    signupのroutesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
+  get 'signup' => 'users#new'
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
