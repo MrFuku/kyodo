2de23b7f1d9641c64b5b56e45ac77896e9210b1b
kawac2236 <kawac2236@gmail.com>
2019-10-27 22:53:22 +0900

    edit,updateメソッドの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,20 @@ class UsersController < ApplicationController
       render("/users/new")
     end
   end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
+  end
 end
