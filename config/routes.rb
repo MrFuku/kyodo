8fa757d78386d59227ee37d0ad7b425749dd4dbb
kawac2236 <kawac2236@gmail.com>
2019-10-27 22:52:51 +0900

    ユーザー編集のrouteの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
   get 'signup' => 'users#new'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
   post 'users/create' => 'users#create'
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
