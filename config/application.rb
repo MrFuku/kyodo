f98e03ad3f82a04e84037d9b8bf2ea5a5cfdc36b
kawac2236 <kawac2236@gmail.com>
2019-10-31 00:02:26 +0900

    javascript(coffee)とテストのファイルの自動生成を抑制

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # generate コマンド実行時の生成物
+    config.generators do |g|
+      # 生成除外
+      g.javascripts false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
