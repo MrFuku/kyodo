278bfcdc4417234bed254099cd4cc8b43af3df2a
u-yah <yuyahayashi3@gmail.com>
2019-10-31 18:23:01 +0900

    ユーザー編集ページに画像投稿機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,11 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
-    @user.image_name = "#{@user.id}.jpg"
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
     if @user.save
       flash[:notice] = 'ユーザー情報を編集しました'
       redirect_to("/users/#{@user.id}")
