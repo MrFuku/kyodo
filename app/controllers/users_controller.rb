5b61e04d976a21c4d40775aa160678f9e3b38f76
kawac2236 <kawac2236@gmail.com>
2019-11-03 21:18:37 +0900

    ログイン機能の追加　セッション情報の格納

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to "/posts/index"
     else
