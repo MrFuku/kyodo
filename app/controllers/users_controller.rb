46c5af46d8b66093d90ec8e1545f6adb4f9ac050
kawac2236 <kawac2236@gmail.com>
2019-11-03 21:11:47 +0900

    パスワード・メールアドレスが見つからない場合の処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,9 @@ class UsersController < ApplicationController
       flash[:notice] = "ログインしました"
       redirect_to "/posts/index"
     else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
       render "users/login_form"
     end
   end
