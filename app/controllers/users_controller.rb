201ac63010e9fa4e948b48e170930dc59cef1cc8
kawac2236 <kawac2236@gmail.com>
2019-11-03 21:41:16 +0900

    新規ユーザー登録時にパスワードを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,11 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: params[:email],
-      image_name: 'default_user.jpg'
+      image_name: 'default_user.jpg',
+      password: params[:password]
     )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = 'ユーザー登録が完了しました'
       redirect_to("/users/#{@user.id}")
     else
