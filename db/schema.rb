df18a886ae57405955b37334d4ebcf68110d87d9
kawac2236 <kawac2236@gmail.com>
2019-11-03 20:27:21 +0900

    パスワード列の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_080327) do
+ActiveRecord::Schema.define(version: 2019_11_02_134840) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_10_31_080327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
