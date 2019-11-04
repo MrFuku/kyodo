9bdf98af19db19736b9506274bc0afc81e0d8970
kawac2236 <kawac2236@gmail.com>
2019-11-04 12:26:24 +0900

    fix: routesの余計な改行を削除

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   post 'login' => 'users#login'
   post 'logout' => 'users#logout'
   
-
   get '/' => 'home#top'
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
