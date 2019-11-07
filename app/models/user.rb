28e9683007c9bd0ebe4bc9ccb0508188406cc861
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-07 14:35:48 +0900

    Userモデルにユーザーに紐づいたPostインスタンスを返すメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true}
+
+  # ユーザーに紐づいたPostインスタンスを戻り値として返す
+  def posts
+    return Post.where(user_id: self.id)
+  end
 end
