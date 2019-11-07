e697382ebc8ef41f4d28acca7b277c6bfbd14cba
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-07 13:27:03 +0900

    postsテーブルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_134840) do
+ActiveRecord::Schema.define(version: 2019_11_07_042508) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
