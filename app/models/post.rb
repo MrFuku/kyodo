2c3902127e6830ce47e4268866ec11d5a9993100
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-07 14:11:24 +0900

    投稿に紐づいたユーザーを返すインスタンスメソッドを定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   validates :content, { presence: true, length: { maximum: 140 } }
   validates :user_id, { presence: true }
+
+  # その投稿に紐づいたユーザーを返すインスタンスメソッド
+  def user
+    return User.find_by(id: self.user_id)
+  end
 end
