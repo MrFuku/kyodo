310f32a47876054cc06f43288a99862804dba96e
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-07 16:32:54 +0900

    user_idカラムが上手く作成されていなかったため、作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_042508) do
+ActiveRecord::Schema.define(version: 2019_11_07_073044) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
