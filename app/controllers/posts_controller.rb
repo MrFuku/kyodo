371fc522864679033279b91c02f1c739790d7a87
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2019-11-07 14:51:14 +0900

    投稿の編集・削除を制限するアクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -46,5 +48,14 @@ class PostsController < ApplicationController
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to('/posts/index')
+  end
+
+  # 投稿に紐づくユーザーと現在ログインしているユーザーが異なるかどうかを比べる
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
 end
