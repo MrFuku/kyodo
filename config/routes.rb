e25b8d337b32ec3617c3321506e908ec02d4b341
u-yah <yuyahayashi3@gmail.com>
2019-11-10 14:55:50 +0900

    createアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
   get 'login' => 'users#login_form'
   post 'login' => 'users#login'
   post 'logout' => 'users#logout'
-  
+  post 'likes/:post_id/create' => 'likes#create'
+
   get '/' => 'home#top'
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
