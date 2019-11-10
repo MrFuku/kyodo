cfbcae20e4e3911223f596d95fb3762e6bf3845d
u-yah <yuyahayashi3@gmail.com>
2019-11-10 14:05:45 +0900

    typo修正

--- db/migrate/20191110041747_create_likes.rb
+++ db/migrate/20191110041747_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
       t.integer :user_id
-      t.interger :post_id
+      t.integer :post_id
 
       t.timestamps
     end
