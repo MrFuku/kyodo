1925878347e625f68d47d14d072431b737f9f85c
u-yah <yuyahayashi3@gmail.com>
2019-11-10 23:29:08 +0900

    「いいね！」をした投稿一覧を表示できる機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,6 +76,8 @@ class UsersController < ApplicationController
   end
 
   def likes
+    @user = User.find_by(id: params[:id])
+    @likes = Like.where(user_id: @user.id)
   end
 
   def ensure_correct_user
