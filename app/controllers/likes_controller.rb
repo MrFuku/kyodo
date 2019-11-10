c277d7bc51b5e820f159b4e56f18b856ace74651
u-yah <yuyahayashi3@gmail.com>
2019-11-10 17:07:54 +0900

    いいね！ボタンの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,5 +2,11 @@ class LikesController < ApplicationController
   before_action :authenticate_user
 
   def create
+    @like = Like.new(
+      user_id: @current_user.id,
+      post_id: params[:post_id]
+    )
+    @like.save
+    redirect_to("/posts/#{params[:post_id]}")
   end
 end
