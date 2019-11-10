f8d313f6ec3c5777ddb64896ab35c66ae452afd4
u-yah <yuyahayashi3@gmail.com>
2019-11-10 17:34:30 +0900

    いいね！の件数を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
     @user = @post.user
+    @likes_count = Like.where(post_id: @post.id).count
   end
 
   def new
